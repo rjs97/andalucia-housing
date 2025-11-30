a5675912a31eac23d5505fa1068a6a35cc564f42
jaya <radhika.jaya4@gmail.com>
2025-11-30 21:28:20 +0100

    fix: remove titles, change to single quotes

--- static_maps.py
+++ static_maps.py
@@ -10,7 +10,7 @@ distrito_malaga = gpd.read_file(r'./shapefiles/malaga_distritos/da_cartografiaDi
 distrito_malaga = distrito_malaga.rename(columns={'NUMERO': 'DISTRITO'})
 
 # VIVIENDAS TURISTICAS
-viviendas_turisticas = pd.read_csv("./data/viviendas_turisticas_nov2024_dist.csv")
+viviendas_turisticas = pd.read_csv('./data/viviendas_turisticas_nov2024_dist.csv')
 
 # filter + sort
 viviendas_turisticas['DISTRITO'] = viviendas_turisticas['CUDIS'] % 100
@@ -30,23 +30,21 @@ distrito_malaga = distrito_malaga.to_crs(epsg=3857)
 # reshape legend
 _, ax = plt.subplots(1,1, figsize=(15,15))
 divider = make_axes_locatable(ax)
-cax = divider.append_axes("bottom", size="3%", pad=0.1)
+cax = divider.append_axes('bottom', size='3%', pad=0.1)
 
 for idx, row in distrito_malaga.iterrows():
     center = row['geometry'].centroid
     ax.text(center.x, center.y, s=row['NAME'], horizontalalignment='center', bbox={'facecolor': 'white', 'alpha':0.8, 'pad': 2, 'edgecolor':'none'})
 
-ax.set_title("Proportion of housing stock used for tourist housing")
-
 vivienda_malaga = distrito_malaga.plot(
-  column="% TOURIST HOUSING", 
+  column='% TOURIST HOUSING', 
   legend= True, 
-  legend_kwds={"label": "% tourist housing", "orientation": "horizontal"},
+  legend_kwds={'label': '% tourist housing', 'orientation': 'horizontal'},
   alpha=0.7, 
   ax=ax,
   cax=cax,
-  cmap="Greys",
-  edgecolor="black"
+  cmap='Greys',
+  edgecolor='black'
 )
 
 vivienda_malaga.set_axis_off()
@@ -56,7 +54,7 @@ cx.add_basemap(vivienda_malaga, source=cx.providers.CartoDB.PositronNoLabels)
 plt.savefig('./website/static/images/maps/vivienda_malaga.jpg')
 
 # AIRBNB MALAGA
-malaga_airbnb = pd.read_csv("./data/malaga_airbnb_20250328.csv")
+malaga_airbnb = pd.read_csv('./data/malaga_airbnb_20250328.csv')
 
 # filter
 malaga_airbnb = malaga_airbnb.dropna(subset=['price','availability_365'])
@@ -74,17 +72,15 @@ malaga_airbnb = malaga_airbnb[malaga_airbnb['room_type'] == 'Entire home/apt'][[
 # create output
 distrito_malaga = distrito_malaga.merge(malaga_airbnb, on='NAME')
 
-ax.set_title("Monthly rent for AirBnB properties")
-
 airbnb_malaga = distrito_malaga.plot(
-  column="PRICE/MONTH (€)", 
+  column='PRICE/MONTH (€)', 
   legend= True, 
-  legend_kwds={"label": "Average monthly price of AirBnBs", "orientation": "horizontal"},
+  legend_kwds={'label': 'Average monthly price of AirBnBs', 'orientation': 'horizontal'},
   alpha=0.7, 
   ax=ax,
   cax=cax,
-  cmap="Greys",
-  edgecolor="black"
+  cmap='Greys',
+  edgecolor='black'
 )
 
 airbnb_malaga.set_axis_off()
